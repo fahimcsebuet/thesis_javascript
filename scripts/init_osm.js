5b848c21568777f1cdc3237825db80b94f541a09
milu <milu@iappdragon.com>
2015-11-16 17:33:05 +0600

    env is ready

--- scripts/init_osm.js
+++ scripts/init_osm.js
@@ -2,3 +2,7 @@ map = new OpenLayers.Map("OSM_Map");
 map.addLayer(new OpenLayers.Layer.OSM());
 var markers = new OpenLayers.Layer.Markers( "Markers" );
 map.addLayer(markers);
+
+ourpoint1 = new OpenLayers.LonLat(90.3989,23.7937);
+ourpoint1.transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject());
+map.setCenter(ourpoint1,12);
