a4f0fef39d1542d0f1b8c20c41c4a347dbcb733e
milu <milu@iappdragon.com>
2015-11-18 15:27:07 +0600

    click event fixed ...

--- scripts/map.js
+++ scripts/map.js
@@ -54,30 +54,4 @@ Map.prototype.createLineString = function(data) {
 	 return linestring;
 };
 
-Map.prototype.getSampleDataforLineString = function() {
-	result= new Array();
-	result[0] = new Array();
-	result[1] = new Array();
-	result[2] = new Array();
-
-	result[0][0] = "1778185456";
-	result[1][result[0][0]] = "90.4179973";
-	result[2][result[0][0]] = "23.7105191";
-
-	result[0][1] = "1778185464";
-	result[1][result[0][1]] = "90.4197997";
-	result[2][result[0][1]] = "23.7063736";
-
-	return result
-};
-
-Map.prototype.getSampleStyle = function() {
-	var style = {
-		strokeColor: '#0000ff',
-		strokeOpacity: 0.8,
-		strokeWidth: 4
-	};
-
-	return style
-};
 
--- scripts/map_click.js
+++ scripts/map_click.js
@@ -1,11 +1,7 @@
 
 Map.prototype.setClickEvents = function() {
 	
-		map = this.osm_map;
-		markers = this.markers;
-		Map_obj = this
-
-
+		that = this;
 		OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
 	    defaultHandlerOptions: {
 	        'single': true,
@@ -30,46 +26,46 @@ Map.prototype.setClickEvents = function() {
 	    }, 
 
 	    trigger: function(e) {
-	        lonlat = map.getLonLatFromPixel(e.xy);
+	        lonlat = that.osm_map.getLonLatFromPixel(e.xy);
 			lonlat.transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
 		
-	        if(Map_obj.select_source==1)
+	        if(that.select_source==1)
 				{
-					Map_obj.s_lat = lonlat.lat;
-					Map_obj.s_lon = lonlat.lon;
+					that.s_lat = lonlat.lat;
+					that.s_lon = lonlat.lon;
 					
-				 var ourpoint1 = new OpenLayers.LonLat(Map_obj.s_lon,Map_obj.s_lat)
-	             ourpoint1.transform(new OpenLayers.Projection("EPSG:4326" ), map.getProjectionObject());
+				 var ourpoint1 = new OpenLayers.LonLat(that.s_lon,that.s_lat);
+	             ourpoint1.transform(new OpenLayers.Projection("EPSG:4326" ), that.osm_map.getProjectionObject());
 				 
-				 markers.addMarker(new OpenLayers.Marker(ourpoint1));
+				 that.markers.addMarker(new OpenLayers.Marker(ourpoint1));
 				 
-				 Map_obj.select_dest=1;
-				 Map_obj.select_source=0;
+				 that.select_dest=1;
+				 that.select_source=0;
 				 
-				 console.log("You have clicked near s (" + Map_obj.s_lat + " N, " + Map_obj.s_lon + " E)");
+				 console.log("You have clicked near s (" + that.s_lat + " N, " + that.s_lon + " E)");
 					
 			}
 			
-			else if(Map_obj.select_dest==1)
+			else if(that.select_dest==1)
 			{
-					Map_obj.d_lat = lonlat.lat;
-					Map_obj.d_lon = lonlat.lon;
+					that.d_lat = lonlat.lat;
+					that.d_lon = lonlat.lon;
 					
-				var ourpoint2 = new OpenLayers.LonLat(Map_obj.d_lon,Map_obj.d_lat)
-	             ourpoint2.transform(new OpenLayers.Projection("EPSG:4326" ), map.getProjectionObject());
+				var ourpoint2 = new OpenLayers.LonLat(that.d_lon,that.d_lat);
+	             ourpoint2.transform(new OpenLayers.Projection("EPSG:4326" ), that.osm_map.getProjectionObject());
 				 
-				 markers.addMarker(new OpenLayers.Marker(ourpoint2));
+				 that.markers.addMarker(new OpenLayers.Marker(ourpoint2));
 				 
-				 console.log("You have clicked near d (" + Map_obj.d_lat + " N, " + Map_obj.d_lon+ " E)");
+				 console.log("You have clicked near d (" + that.d_lat + " N, " + that.d_lon+ " E)");
 
-				 Map_obj.select_dest = 0;
+				 that.select_dest = 0;
 			}
 			else
 			{
-				Map_obj.select_dest=0;
-				Map_obj.select_source=1;
+				that.select_dest=0;
+				that.select_source=1;
 
-				markers.clearMarkers();
+				that.markers.clearMarkers();
 			
 			
 			}
@@ -78,6 +74,6 @@ Map.prototype.setClickEvents = function() {
 	});
 
 	var click = new OpenLayers.Control.Click();
-	map.addControl(click);
+	that.osm_map.addControl(click);
 	click.activate();
 };
