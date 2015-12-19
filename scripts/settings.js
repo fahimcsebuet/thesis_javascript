d47b5f1eda96ec3cad46ac5bea07e56940077a56
Abu Reyan Ahmed <abureyanahmed@user-HP-Pro-2000-Microtower-PC.(none)>
2015-12-19 14:29:46 +0600

    buet cse site

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,6 +1,6 @@
-edge_list_url = 'http://fahimcsebuet.github.io/thesis_javascript/graph/edge_list.txt';
-edge_list_dist_url = 'http://fahimcsebuet.github.io/thesis_javascript/graph/edge_list_dist.txt';
-node_list_url = 'http://fahimcsebuet.github.io/thesis_javascript/graph/node_list.txt';
+edge_list_url = 'https://fahimcsebuet.github.io/thesis_javascript/graph/edge_list.txt';
+edge_list_dist_url = 'https://fahimcsebuet.github.io/thesis_javascript/graph/edge_list_dist.txt';
+node_list_url = 'https://fahimcsebuet.github.io/thesis_javascript/graph/node_list.txt';
 
 global_graph = null;
 algo_status = null;
